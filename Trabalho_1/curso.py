3ac39543ea9630831e7b49e658f55737ae7fd1a7
Ezequiel Grassi <ezequiel.oliveira@tinotech.com.br>
2023-11-09 20:38:41 -0300

    initial

--- Trabalho_1/curso.py
+++
@@ -1,7 +0,0 @@
-class Curso:
-
-    def __init__(self, nome, carga_horaria, tipo, pessoa):
-        self._nome = nome
-        self._carga_horaria = carga_horaria
-        self._tipo = tipo
-        self._pessoa = pessoa
--- Trabalho_2/order.py
+++
@@ -1,6 +0,0 @@
-class Order:
-
-    def __init__(self, total_price, status, client):
-        self._total_price = total_price
-        self._status = status
-        self._client = client
