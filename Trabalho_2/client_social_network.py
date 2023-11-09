c8c00e4caa23ca20d594764647dfffcdd3752717
Ezequiel Grassi <ezequiel.oliveira@tinotech.com.br>
2023-11-09 20:46:58 -0300

    initial

--- Trabalho_2/client_social_network.py
+++
@@ -1,5 +0,0 @@
-class ClientSocialNetwork:
-
-    def __init__(self, client, socialNetwork):
-        self._client = client
-        self._socialNetwork = socialNetwork
--- Trabalho_2/social_network.py
+++
@@ -1,5 +0,0 @@
-class SocialNetwork:
-
-    def __init__(self, name, description):
-        self._name = name
-        self._description = description
