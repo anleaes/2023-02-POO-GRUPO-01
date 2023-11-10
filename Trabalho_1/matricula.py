219a6998c00c88f550152134c84f801144734610
Ezequiel Grassi <ezequiel.oliveira@tinotech.com.br>
2023-11-10 08:35:43 -0300

    ajuste matricula

--- Trabalho_1/matricula.py
+++ Trabalho_1/matricula.py
@@ -1,15 +1,15 @@
+from curso import Curso
+from pessoa_fisica import PessoaFisica
+from pessoa_juridica import PessoaJuridica
 class Matricula:
 
-    def __init__(self, numero_matricula, data_matricula, curso, aluno, contratante):
+    def __init__(self, numero_matricula, data_matricula, curso = Curso, aluno = PessoaFisica, contratante = PessoaJuridica):
         self._numero_matricula = numero_matricula
         self._data_matricula = data_matricula
         self._curso = curso
         self._aluno = aluno
         self._contratante = contratante
 
-    def criarMatricula(numero_matricula, data_matricula, curso, aluno, contratante):
-        Matricula._aluno = aluno
-        Matricula._contratante = contratante
-        Matricula._curso = curso
-        Matricula._data_matricula = data_matricula
-        Matricula._numero_matricula = numero_matricula
+    def exibirMatricula(self):
+        print(f"Matricula: {self._numero_matricula} \nCurso: {self._curso._nome} \nAluno: {self._aluno._nome} \nContratante: {self._contratante._razao_social}")
+        
