d5eb237d6ae2fdb0114cf440492d324345d95dd1
Ezequiel Grassi <ezequiel.oliveira@tinotech.com.br>
2023-12-02 14:13:46 -0300

    create tutor view

--- projetoapp/apps/tutor/serializer.py
+++ projetoapp/apps/tutor/serializer.py
@@ -1,7 +1,7 @@
 from .models import Tutor
 from rest_framework import serializers
 
-class CategorySerializer(serializers.ModelSerializer):
+class TutorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tutor
         fields = '__all__'
