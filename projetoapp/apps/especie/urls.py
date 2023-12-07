8cfeb4fcab2fe75d344ac4fabbdea6131ce95c31
Almir Dessbesel <xstaybrutalx@live.com>
2023-12-07 14:05:24 -0300

    n2_almir_especie_url

---
+++ projetoapp/apps/especie/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+app_name = 'especies'
+
+router = routers.DefaultRouter()
+router.register('', views.EspecieViewSet, basename='especies')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
